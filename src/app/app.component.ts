b63116f81d94a87b8101f1007efc2e6376d9a1a0
Esteban Piedrahita <estebanpiedra89@gmail.com>
2021-12-19 12:29:44 -0500

    add delete function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
-
+import { Employee } from './models/employee';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-crud-sample';
+
+  employeeArray: Employee[] = [
+    {id: 1, name: "Ryan", country: "USA"},
+    {id: 2, name: "Sara", country: "COL"},
+    {id: 3, name: "Camila", country: "PER"},
+  ];
+  
+  selectedEmployee: Employee = new Employee();
+
+  openForEdit(employee: Employee){
+    this.selectedEmployee = employee;
+  }
+
+  addOrEdit() {
+    if(this.selectedEmployee.id == 0){
+      this.selectedEmployee.id = this.employeeArray.length + 1;
+      this.employeeArray.push(this.selectedEmployee);
+    }
+
+    this.selectedEmployee = new Employee();
+  }
+
 }
